1b189f60e73a9fcf1eba121f32e266716863b32b
kovakimy <kristina.ovakimyan@intel.com>
2021-02-02 22:14:33 +0300

    add det_net ver 1.2

--- include/detector.hpp
+++ include/detector.hpp
@@ -4,11 +4,13 @@
 #include <opencv2/core.hpp>
 
 
-struct DetectionObject {
+struct DetectionObject
+{
     int xmin, ymin, xmax, ymax, class_id;
     float confidence;
 
-    DetectionObject(int class_id, double score, double x0, double y0, double x1, double y1) {
+    DetectionObject(int class_id, double score, double x0, double y0, double x1, double y1)
+    {
         this->xmin = static_cast<int>(x0);
         this->ymin = static_cast<int>(y0);
         this->xmax = static_cast<int>(x1);
@@ -21,9 +23,9 @@ struct DetectionObject {
 class Detector
 {
 public:
-	InferenceEngine::Core ie;
-	InferenceEngine::CNNNetwork cnnNetwork;
-	std::string modelPath, configPath;
-	Detector(std::string &modelPath, std::string &configPath);
+    InferenceEngine::Core ie;
+    InferenceEngine::CNNNetwork cnnNetwork;
+    std::string modelPath, configPath;
+    Detector(std::string &modelPath, std::string &configPath);
     std::vector<DetectionObject> getDetections(cv::Mat &image);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,59 +6,60 @@
 #define pd 60
 
 void progressBar(double progress) {
-	int val = (int)(progress * 100);
-	int left = (int)(progress * pd);
-	int rigth = pd - left;
-	printf("\r%3d%% [%.*s%*s]", val, left, str, rigth, "");
-	fflush(stdout);
+    int val = (int)(progress * 100);
+    int left = (int)(progress * pd);
+    int rigth = pd - left;
+    printf("\r%3d%% [%.*s%*s]", val, left, str, rigth, "");
+    fflush(stdout);
 }
 
-cv::Mat draw_bbox(cv::Mat &frame, std::vector<DetectionObject>& objects) {
-	for (int i = 0; i < objects.size(); i++) {
-		if (objects[i].confidence > 0.75) 
-			cv::rectangle(frame, cv::Rect(cv::Point(objects[i].xmin, objects[i].ymin), cv::Point(objects[i].xmax, objects[i].ymax)), cv::Scalar(225, 0, 0));
-	}
-	return frame;
+cv::Mat draw_bbox(cv::Mat &frame, std::vector<DetectionObject>& objects)
+{
+    for (int i = 0; i < objects.size(); i++)
+    {
+        if (objects[i].confidence > 0.75) 
+            cv::rectangle(frame, cv::Rect(cv::Point(objects[i].xmin, objects[i].ymin), cv::Point(objects[i].xmax, objects[i].ymax)), cv::Scalar(225, 0, 0));
+    }
+    return frame;
 }
 
 int main()
 {
-	InferenceEngine::Core ie;
-	std::string FLAGS_m = "C://Users//kovakimy//models//pedestrian-detection-adas-0002.xml";
-	std::string FLAGS_c = "C://Users//kovakimy//models//pedestrian-detection-adas-0002.bin";
-	std::string FLAGS_v = "C://Users//kovakimy//models//people-detection.mp4";
+    InferenceEngine::Core ie;
+    std::string FLAGS_m = "C://Users//kovakimy//models//pedestrian-detection-adas-0002.xml";
+    std::string FLAGS_c = "C://Users//kovakimy//models//pedestrian-detection-adas-0002.bin";
+    std::string FLAGS_v = "C://Users//kovakimy//models//people-detection.mp4";
 
-	Detector detector(FLAGS_m, FLAGS_c);
+    Detector detector(FLAGS_m, FLAGS_c);
 
-	int frame_counter = 1;
-	cv::Mat frame;
-	cv::Mat result;
-	cv::VideoCapture capture(FLAGS_v);
+    int frame_counter = 1;
+    cv::Mat frame;
+    cv::Mat result;
+    cv::VideoCapture capture(FLAGS_v);
 
-	double frame_width = capture.get(cv::CAP_PROP_FRAME_WIDTH);
-	double frame_height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+    double frame_width = capture.get(cv::CAP_PROP_FRAME_WIDTH);
+    double frame_height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
 
-	cv::VideoWriter out("C:/Users/kovakimy/OneDrive - Intel Corporation/Pictures/intel_project/demos/build/out.avi",
-		cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frame_width, frame_height), true);
+    cv::VideoWriter out("C:/Users/kovakimy/OneDrive - Intel Corporation/Pictures/intel_project/demos/build/out.avi",
+    cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frame_width, frame_height), true);
 
-	std::cout << "Progress bar..." << std::endl;
-	while (frame_counter < capture.get(cv::CAP_PROP_FRAME_COUNT)) {
-
-		progressBar((frame_counter + 1) / capture.get(cv::CAP_PROP_FRAME_COUNT));
-		capture >> frame;
-
-		if (frame.empty()) {
-			frame_counter++;
-			continue;
-		}
-		std::vector<DetectionObject> detections = detector.getDetections(frame);
-		result = draw_bbox(frame, detections);
-		out.write(result);
-		frame_counter++;
-		capture >> frame;
-	}
-	std::cout << std::endl;
-	std::cout << "Completed";
-	return 0;
+    std::cout << "Progress bar..." << std::endl;
+    while (frame_counter < capture.get(cv::CAP_PROP_FRAME_COUNT))
+    {
+        progressBar((frame_counter + 1) / capture.get(cv::CAP_PROP_FRAME_COUNT));
+        capture >> frame;
+        if (frame.empty())
+        {
+            frame_counter++;
+            continue;
+        }
+        std::vector<DetectionObject> detections = detector.getDetections(frame);
+        result = draw_bbox(frame, detections);
+        out.write(result);
+        frame_counter++;
+        capture >> frame;
+    }
+    std::cout << std::endl;
+    std::cout << "Completed";
+    return 0;
 }
-
